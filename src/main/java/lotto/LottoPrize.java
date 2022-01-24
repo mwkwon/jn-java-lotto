c737c71ca2d417e3891ed3ff9d0fda212a2d4d54
정주현 <juhyeon@joonggonara.co.kr>
2022-01-24 18:24:44 +0900

    refactor: 로또 결과 if else -> stream 변경

--- src/main/java/lotto/LottoPrize.java
+++ src/main/java/lotto/LottoPrize.java
@@ -1,5 +1,7 @@
 package lotto;
 
+import java.util.Arrays;
+
 public enum LottoPrize {
     NOTHING(0, 0),
     FOURTH_PRIZE(3, 5_000),
@@ -7,24 +9,18 @@ public enum LottoPrize {
     SECOND_PRIZE(5, 1_500_000),
     FIRST_PRIZE(6, 2_000_000_000);
 
-    public final int matchCount;
-    public final int prize;
+    public final Integer matchCount;
+    public final Integer prize;
 
     LottoPrize(int matchCount, int prize) {
         this.matchCount = matchCount;
         this.prize = prize;
     }
 
-    public static LottoPrize findLottoPrize(int matchCount) {
-        if (matchCount == FIRST_PRIZE.matchCount) {
-            return FIRST_PRIZE;
-        } else if (matchCount == SECOND_PRIZE.matchCount) {
-            return SECOND_PRIZE;
-        } else if (matchCount == THIRD_PRIZE.matchCount) {
-            return THIRD_PRIZE;
-        } else if (matchCount == FOURTH_PRIZE.matchCount) {
-            return FOURTH_PRIZE;
-        }
-        return NOTHING;
+    public static LottoPrize findLottoPrize(Integer matchCount) {
+        return Arrays.stream(LottoPrize.values())
+                .filter(e -> e.matchCount.equals(matchCount))
+                .findFirst()
+                .orElse(LottoPrize.NOTHING);
     }
 }
